fe515871ebb29df2751e6c09ec1b35fded9244c2
Ruth Onuoha <onuoharuth1@gmail.com>
2023-11-21 12:57:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const { ApolloServer } = require("apollo-server");
+const { importSchema } = require("graphql-import");
+const EtherDataSource = require("./datasource/ethDatasource");
+const typeDefs = importSchema("./schema.graphql");
+
+require("dotenv").config();
+
+const resolvers = {
+  Query: {
+    // Get ether balance for an address
+    etherBalanceByAddress: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.etherBalanceByAddress(),
+
+    // Get total ether supply
+    totalSupplyOfEther: (root, _args, { dataSources }) =>  
+      dataSources.ethDataSource.totalSupplyOfEther(),
+
+    // Get latest ethereum price
+    latestEthereumPrice: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    // Get block confirmation time
+    blockConfirmationTime: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getBlockConfirmationTime(),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    ethDataSource: new EtherDataSource(),
+  }),
+});
+
+server.timeout = 0;
+server.listen("9000").then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
